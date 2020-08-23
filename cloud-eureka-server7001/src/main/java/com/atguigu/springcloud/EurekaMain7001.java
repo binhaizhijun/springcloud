c0a05e912d1010a221e6d29901b237ece463e1ab
binhaizhijun <binhaizhijun@163.com>
2020-08-23 22:03:37 +0800

    构建EurekaServer注册中心,将服务注册到注册中心

--- cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -2,20 +2,20 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @ProjectName: cloud2020
  * @Package: com.atguigu.springcloud
- * @ClassName: OrderMain80
+ * @ClassName: EurekaMain7001
  * @Description: java类作用描述
  * @Author: 滨海之君
- * @CreateDate: 2020/8/23 13:46
+ * @CreateDate: 2020/8/23 20:26
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer  //声明这是一个Eureka服务端
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class,args);
+        SpringApplication.run(EurekaMain7001.class,args);
     }
 }
