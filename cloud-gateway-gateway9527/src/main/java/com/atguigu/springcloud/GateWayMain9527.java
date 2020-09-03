df7c9de5cc6eee028bdb5a0af6b3bd6d397ff210
binhaizhijun <binhaizhijun@163.com>
2020-09-04 00:36:57 +0800

    Gateway

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @ProjectName: cloud2020
+ * @Package: com.atguigu.springcloud
+ * @ClassName: GateWayMain9527
+ * @Description: java类作用描述
+ * @Author: 滨海之君
+ * @CreateDate: 2020/9/3 20:42
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+}
---
+++ cloud-gateway-gateway9527/src/test/java/T1.java
@@ -0,0 +1,16 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @ProjectName: cloud2020
+ * @Package: PACKAGE_NAME
+ * @ClassName: T1
+ * @Description: java类作用描述
+ * @Author: 滨海之君
+ * @CreateDate: 2020/9/3 23:17
+ */
+public class T1 {
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+    }
+}
